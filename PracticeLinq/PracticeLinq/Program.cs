d6ff78f42d78261af38655f42ad2dc68a072e395
U-DESKTOP-1TUJ31R\iisus <comp.structuralbiology@gmail.com>
2020-07-16 13:32:09 -0400

    Pracitce Linq

---
+++ PracticeLinq/PracticeLinq/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cars = new string[] { "Bugatti", "Knight XV", "Cardillac", "BMW", "Tesla" };
+
+            LinqQuerySyntax(cars);
+            LinqMethodSyntax(cars);
+
+        }
+
+        public static void LinqQuerySyntax(string[] a)
+        {
+            var startswithA = from x in a
+                              where x.ToLower().Contains("b")
+                              orderby x descending
+                              select x;
+
+            foreach (var letterA in startswithA)
+            {
+                Console.WriteLine($"{letterA}");
+            }
+        }
+
+        public static void LinqMethodSyntax(string[] a)
+        {
+            var startswithA = a.Where(x => x.ToLower().Contains("b"));
+            foreach (var letterA in startswithA)
+            {
+                Console.WriteLine($"{letterA}");
+            }
+        }
+    }
+}
